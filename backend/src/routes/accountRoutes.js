036a16a31e3bdcb764e6d7ad515cd36f8d7c149b
nate <ryrgnx@gmail.com>
2026-01-08 23:29:13 +0800

    oops forgot to remove the get call

--- backend/src/routes/accountRoutes.js
+++ backend/src/routes/accountRoutes.js
@@ -3,7 +3,6 @@ import { signin, signup } from "../controllers/accountController.js";
 
 const router = express.Router();
 
-router.get("/", accountController);
 router.post("/signin", signin);
 router.post("/signup", signup);
 
